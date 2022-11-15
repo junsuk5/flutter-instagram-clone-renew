55ac73cd0bfbf0b527c2083a884b605ca4eee570
Junsuk Oh <junsuk.oh@survivalcoding.com>
2022-11-15 10:15:21 +0900

    화면 설계, TapPage 작성

---
+++ android/app/src/main/kotlin/com/jsoh/instagram_clone_renew/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.jsoh.instagram_clone_renew
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/tab/tab_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class TabPage extends StatefulWidget {
+  const TabPage({Key? key}) : super(key: key);
+
+  @override
+  State<TabPage> createState() => _TabPageState();
+}
+
+class _TabPageState extends State<TabPage> {
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Account',
+          ),
+        ],
+      ),
+    );
+  }
+}
