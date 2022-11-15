2aaa9fe75ad956693b7b2a7168b6163eec5bd489
Junsuk Oh <junsuk.oh@survivalcoding.com>
2022-11-15 10:53:49 +0900

    HomePage UI 작성

--- lib/tab/home/home_page.dart
+++ lib/tab/home/home_page.dart
@@ -9,10 +9,75 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Instagram Clone'),
       ),
-      body: Column(
-        children: const [
-          Text('Instagram 에 오신 것을 환영합니다')
-        ],
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(height: 20),
+            const Text(
+              'Instagram 에 오신 것을 환영합니다',
+              style: TextStyle(fontSize: 24),
+            ),
+            const SizedBox(height: 20),
+            const Text('사진과 동영상을 보려면 팔로우하세요'),
+            const SizedBox(height: 20),
+            Card(
+              elevation: 4.0,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      width: 80,
+                      height: 80,
+                      child: CircleAvatar(
+                        backgroundImage: NetworkImage('https://pbs.twimg.com/profile_images/1374979417915547648/vKspl9Et_400x400.jpg'),
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    const Text('test@test.com',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold
+                      ),
+                    ),
+                    const Text('닉네임'),
+                    const SizedBox(height: 8),
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.network('https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/202207/31/d399eb2b-154e-4585-bdbb-b8a86939577c.jpg',
+                          width: 70,
+                          height: 70,
+                          fit: BoxFit.cover,
+                        ),
+                        const SizedBox(width: 4),
+                        Image.network('http://talkimg.imbc.com/TVianUpload/tvian/TViews/image/2022/01/05/0vU4v7vCfjju637769738893922804.jpg',
+                          width: 70,
+                          height: 70,
+                          fit: BoxFit.cover,
+                        ),
+                        const SizedBox(width: 4),
+                        Image.network('https://pbs.twimg.com/profile_images/1374979417915547648/vKspl9Et_400x400.jpg',
+                          width: 70,
+                          height: 70,
+                          fit: BoxFit.cover,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 8),
+                    const Text('Facebook 친구'),
+                    const SizedBox(height: 8),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: const Text('팔로우'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
