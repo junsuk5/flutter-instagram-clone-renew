5a484329e40a490051e89824cc7ee6396363a94b
Junsuk Oh <junsuk.oh@survivalcoding.com>
2022-11-15 11:19:09 +0900

    AccountPage UI 작성

--- lib/tab/account/account_page.dart
+++ lib/tab/account/account_page.dart
@@ -5,6 +5,110 @@ class AccountPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Instagram Clone'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.exit_to_app),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                Stack(
+                  children: [
+                    const SizedBox(
+                      width: 80,
+                      height: 80,
+                      child: CircleAvatar(
+                        backgroundImage: NetworkImage(
+                            'https://pbs.twimg.com/profile_images/1374979417915547648/vKspl9Et_400x400.jpg'),
+                      ),
+                    ),
+                    Container(
+                      width: 80,
+                      height: 80,
+                      alignment: Alignment.bottomRight,
+                      child: Stack(
+                        alignment: Alignment.center,
+                        children: const [
+                          SizedBox(
+                            width: 28,
+                            height: 28,
+                            child: FloatingActionButton(
+                              onPressed: null,
+                              backgroundColor: Colors.white,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 25,
+                            height: 25,
+                            child: FloatingActionButton(
+                              onPressed: null,
+                              child: Icon(Icons.add),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 8),
+                const Text(
+                  '아이유',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )
+              ],
+            ),
+            Column(
+              children: const [
+                Text(
+                  '3',
+                  style: TextStyle(fontSize: 18),
+                ),
+                Text(
+                  '게시물',
+                  style: TextStyle(fontSize: 18),
+                ),
+              ],
+            ),
+            Column(
+              children: const [
+                Text(
+                  '0',
+                  style: TextStyle(fontSize: 18),
+                ),
+                Text(
+                  '팔로워',
+                  style: TextStyle(fontSize: 18),
+                ),
+              ],
+            ),
+            Column(
+              children: const [
+                Text(
+                  '0',
+                  style: TextStyle(fontSize: 18),
+                ),
+                Text(
+                  '팔로잉',
+                  style: TextStyle(fontSize: 18),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
