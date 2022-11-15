2b8f7fe6b4946f0d78c60deb0a6249fd9738833d
Junsuk Oh <junsuk.oh@survivalcoding.com>
2022-11-15 12:53:21 +0900

    ImagePicker 라이브러리 연동

---
+++ lib/create/create_model.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+import 'package:image_picker/image_picker.dart';
+
+class CreateModel {
+  final _picker = ImagePicker();
+
+  Future<File?> getImage() async {
+    final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+
+    if (image == null) {
+      return null;
+    }
+
+    return File(image.path);
+  }
+}
--- lib/create/create_page.dart
+++ lib/create/create_page.dart
@@ -1,7 +1,19 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class CreatePage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:instagram_clone_renew/create/create_model.dart';
+
+class CreatePage extends StatefulWidget {
   const CreatePage({Key? key}) : super(key: key);
+
+  @override
+  State<CreatePage> createState() => _CreatePageState();
+}
+
+class _CreatePageState extends State<CreatePage> {
+  final model = CreateModel();
+
+  File? _image;
 
   @override
   Widget build(BuildContext context) {
@@ -10,7 +22,9 @@ class CreatePage extends StatelessWidget {
         title: const Text('새 게시물'),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: () {
+              // 이미지 피커 실행
+            },
             icon: const Icon(Icons.send),
           ),
         ],
@@ -32,13 +46,19 @@ class CreatePage extends StatelessWidget {
               ),
               const SizedBox(height: 20),
               ElevatedButton(
-                onPressed: () {},
+                onPressed: () async {
+                  _image = await model.getImage();
+
+                  // 화면 갱신
+                  setState(() {});
+                },
                 child: const Text('이미지 선택'),
               ),
-              Image.network(
-                'http://talkimg.imbc.com/TVianUpload/tvian/TViews/image/2022/01/05/0vU4v7vCfjju637769738893922804.jpg',
-                width: 300,
-              ),
+              if (_image != null)
+                Image.file(
+                  _image!,
+                  width: 300,
+                ),
             ],
           ),
         ),
