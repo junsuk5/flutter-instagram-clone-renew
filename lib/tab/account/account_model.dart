82e81702dbb6573d0e640edacefc4a8948c2346c
Junsuk Oh <junsuk.oh@survivalcoding.com>
2022-11-15 22:36:02 +0900

    내 사진만 보이게 하기

--- lib/tab/account/account_model.dart
+++ lib/tab/account/account_model.dart
@@ -1,6 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
+import '../../domain/post.dart';
+
 class AccountModel {
+  final Stream<QuerySnapshot<Post>> postsStream = FirebaseFirestore.instance
+      .collection('posts')
+      .where('userId', isEqualTo: FirebaseAuth.instance.currentUser?.uid)
+      .withConverter<Post>(
+        fromFirestore: (snapshot, _) => Post.fromJson(snapshot.data()!),
+        toFirestore: (post, _) => post.toJson(),
+      )
+      .snapshots();
+
   void logout() async {
     await FirebaseAuth.instance.signOut();
   }
--- lib/tab/account/account_page.dart
+++ lib/tab/account/account_page.dart
@@ -1,5 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
+import '../../detail_post/detail_post_page.dart';
+import '../../domain/post.dart';
 import 'account_model.dart';
 
 class AccountPage extends StatelessWidget {
@@ -22,94 +25,146 @@ class AccountPage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: Column(
           children: [
-            Column(
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Stack(
+                Column(
                   children: [
-                    SizedBox(
-                      width: 80,
-                      height: 80,
-                      child: CircleAvatar(
-                        backgroundImage:
-                            NetworkImage(model.getProfileImageUrl()),
-                      ),
+                    Stack(
+                      children: [
+                        SizedBox(
+                          width: 80,
+                          height: 80,
+                          child: CircleAvatar(
+                            backgroundImage:
+                                NetworkImage(model.getProfileImageUrl()),
+                          ),
+                        ),
+                        Container(
+                          width: 80,
+                          height: 80,
+                          alignment: Alignment.bottomRight,
+                          child: Stack(
+                            alignment: Alignment.center,
+                            children: const [
+                              SizedBox(
+                                width: 28,
+                                height: 28,
+                                child: FloatingActionButton(
+                                  onPressed: null,
+                                  backgroundColor: Colors.white,
+                                ),
+                              ),
+                              SizedBox(
+                                width: 25,
+                                height: 25,
+                                child: FloatingActionButton(
+                                  onPressed: null,
+                                  child: Icon(Icons.add),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
-                    Container(
-                      width: 80,
-                      height: 80,
-                      alignment: Alignment.bottomRight,
-                      child: Stack(
-                        alignment: Alignment.center,
-                        children: const [
-                          SizedBox(
-                            width: 28,
-                            height: 28,
-                            child: FloatingActionButton(
-                              onPressed: null,
-                              backgroundColor: Colors.white,
-                            ),
-                          ),
-                          SizedBox(
-                            width: 25,
-                            height: 25,
-                            child: FloatingActionButton(
-                              onPressed: null,
-                              child: Icon(Icons.add),
-                            ),
-                          ),
-                        ],
+                    const SizedBox(height: 8),
+                    Text(
+                      model.getNickName(),
+                      style: const TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
                       ),
+                    )
+                  ],
+                ),
+                Column(
+                  children: const [
+                    Text(
+                      '3',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    Text(
+                      '게시물',
+                      style: TextStyle(fontSize: 18),
                     ),
                   ],
                 ),
-                const SizedBox(height: 8),
-                Text(
-                  model.getNickName(),
-                  style: const TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                  ),
-                )
-              ],
-            ),
-            Column(
-              children: const [
-                Text(
-                  '3',
-                  style: TextStyle(fontSize: 18),
+                Column(
+                  children: const [
+                    Text(
+                      '0',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    Text(
+                      '팔로워',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ],
                 ),
-                Text(
-                  '게시물',
-                  style: TextStyle(fontSize: 18),
-                ),
-              ],
-            ),
-            Column(
-              children: const [
-                Text(
-                  '0',
-                  style: TextStyle(fontSize: 18),
-                ),
-                Text(
-                  '팔로워',
-                  style: TextStyle(fontSize: 18),
+                Column(
+                  children: const [
+                    Text(
+                      '0',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    Text(
+                      '팔로잉',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ],
                 ),
               ],
             ),
-            Column(
-              children: const [
-                Text(
-                  '0',
-                  style: TextStyle(fontSize: 18),
-                ),
-                Text(
-                  '팔로잉',
-                  style: TextStyle(fontSize: 18),
-                ),
-              ],
+            const SizedBox(height: 20),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(2.0),
+                child: StreamBuilder<QuerySnapshot<Post>>(
+                    stream: model.postsStream,
+                    builder: (context, snapshot) {
+                      if (snapshot.hasError) {
+                        return const Text('알 수 없는 에러');
+                      }
+
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(child: CircularProgressIndicator());
+                      }
+
+                      List<Post> posts =
+                      snapshot.data!.docs.map((e) => e.data()).toList();
+
+                      return GridView.builder(
+                        itemCount: posts.length,
+                        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 3,
+                          mainAxisSpacing: 2.0,
+                          crossAxisSpacing: 2.0,
+                        ),
+                        itemBuilder: (BuildContext context, int index) {
+                          final post = posts[index];
+                          return GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => DetailPostPage(post: post)),
+                              );
+                            },
+                            child: Hero(
+                              tag: post.id,
+                              child: Image.network(
+                                post.imageUrl,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    }),
+              ),
             ),
           ],
         ),
