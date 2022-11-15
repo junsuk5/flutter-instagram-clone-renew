2f266ed926880f78581580986b08710269d1f7a4
Junsuk Oh <junsuk.oh@survivalcoding.com>
2022-11-15 12:07:06 +0900

    SearchPage UI 작성

--- lib/tab/search/search_page.dart
+++ lib/tab/search/search_page.dart
@@ -3,8 +3,40 @@ import 'package:flutter/material.dart';
 class SearchPage extends StatelessWidget {
   const SearchPage({Key? key}) : super(key: key);
 
+  final List<String> _urls = const [
+    'https://pbs.twimg.com/profile_images/1374979417915547648/vKspl9Et_400x400.jpg',
+    'http://talkimg.imbc.com/TVianUpload/tvian/TViews/image/2022/01/05/0vU4v7vCfjju637769738893922804.jpg',
+    'https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/202207/31/d399eb2b-154e-4585-bdbb-b8a86939577c.jpg',
+    'https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/202207/31/d399eb2b-154e-4585-bdbb-b8a86939577c.jpg',
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(onPressed: () {  },
+        child: const Icon(Icons.create),
+      ),
+      appBar: AppBar(
+        title: const Text('Instagram clone'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(2.0),
+        child: GridView.builder(
+          itemCount: _urls.length,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            mainAxisSpacing: 2.0,
+            crossAxisSpacing: 2.0,
+          ),
+          itemBuilder: (BuildContext context, int index) {
+            final url = _urls[index];
+            return Image.network(
+              url,
+              fit: BoxFit.cover,
+            );
+          },
+        ),
+      ),
+    );
   }
 }
